bd4481cb4d1a9807f10a42565d81bec4043f65d0
Omer Shapira <info@omershapira.com>
2015-01-11 23:09:08 -0500

    moves to generators, adds token filters

--- src/parser.py
+++ src/parser.py
@@ -3,53 +3,56 @@ import json
 import re
 import operator
 from urlparse import urlsplit
-import logging
-from itertools import imap, ifilter, chain
+from itertools import imap, ifilter
 from collections import Counter
 from pprint import pprint 
 
 from sys import argv
 
 def get_tokens(s, prune=True):
-	#remove ['.html', '.html', '.php', '.py', 'erb'] etc
+	## remove ['.html', '.html', '.php', '.py', 'erb'] etc
 	pruned = re.sub(r'\.\w{1,4}$', '', s, flags=re.IGNORECASE) if prune else s
 	pre_split = re.split(r'\W', pruned)
-	#remove empty strings
+	## remove empty strings
 	clean_split = ifilter (lambda x : x, pre_split) 
 	return set(clean_split)
 
 
 class NoticeProcessor(object):
-	def __init__(self, notice, token_threshold=5, common_proportion=0.25, diversity_threshold = 0.2):
+	def __init__(self, notice, token_threshold=5, common_proportion=0.25, diversity_threshold = 0.2, rules=None):
 		self.notice = notice
 		self.token_threshold = token_threshold
 		self.common_proportion = common_proportion
 		self.diversity_threshold = diversity_threshold
-
+		self.rules = rules or {'disallowed_tokens':['index', 'rar', 'download', 'pirate', 'mp3', 'pdf', 'torrent', 'file', '4shared']}
 
 	def apply_token_threshold(self, col):
 		try:
 			max_num = col.most_common(1)[0][1]
-			large_enough = filter(lambda x : x[1] >= max(self.token_threshold, max_num * self.common_proportion), col.viewitems())
+			
+			def filter_func(x):
+				cond = (x[1] >= max(self.token_threshold, max_num * self.common_proportion)) and (x[0] not in self.rules['disallowed_tokens'])
+				return cond
+
+			large_enough = filter(filter_func, col.viewitems())
 			return large_enough
 		except Exception as e:
-			# raise(e)
 			return []
 
 
-	def get_report(self):
+	def make_report(self):
 		''' 
-		we first want to see that the domains 
+		we want to see that the domains 
 		in the list are varied (over space or over time.)
+		First we deal with space.
 		'''
-		reports = []
-		
 		for work in self.notice['dmca']['works']:
 			try:
 				report = {}
 				urls = (x['url'] for x in work['infringing_urls'])
 				parsed_urls = map(urlsplit, urls)
 
+				## Calculate Tokens
 				extract_tokens = lambda x : get_tokens(x.path) | get_tokens(x.query)
 				token_list = (extract_tokens(x) for x in parsed_urls)	
 				all_tokens = reduce(operator.add, imap(Counter, token_list), Counter())
@@ -60,6 +63,7 @@ class NoticeProcessor(object):
 				else: 
 					report['common_tokens'] = common_tokens
 
+				## Calculate Diversity
 				all_sites = [x.hostname for x in parsed_urls]
 				num_sites, num_urls = len(set(all_sites)), len(all_sites) 
 				diversity = num_sites / num_urls
@@ -71,11 +75,14 @@ class NoticeProcessor(object):
 					report['num_sites'] = num_sites
 					report['diversity'] = diversity
 
-				reports.append(report)
+				yield report
 			except Exception as e:
 				print (e)
-			
-		return reports
+
+
+	def get_report(self):
+		return list(self.make_report())
+
 
 def main():
 	filename = argv[1]
