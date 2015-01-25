b92881a7f972fafc8ed6d82487f9e7aa3ef1ca09
Omer Shapira <info@omershapira.com>
2015-01-25 17:45:39 -0500

    Working request daemon

--- src/RequestDaemon.py
+++ src/RequestDaemon.py
@@ -2,9 +2,9 @@ import pycurl
 import logging
 import ijson
 import itertools
-from io import StringIO
+from io import BytesIO, StringIO
 import time
-import parser
+import RequestParser
 
 curl = pycurl.Curl()
 URL_ROOT = "https://chillingeffects.org/"
@@ -14,37 +14,39 @@ PATH_SEARCH = "notices/search"
 
 
 class Request(object):
-    
+
     def __init__(self, **kwargs):
         self.params = kwargs
 
     def execute(self, request_next_page=True):
         # TODO: Make requests, parse page information from JSON, recursively make more requests    
-        self.buffer = StringIO()
-        if not self.curl:
+        self.buffer = BytesIO()
+        if not hasattr(self, "curl"):
             self.curl = pycurl.Curl()
             self.curl.setopt(pycurl.HTTPHEADER, [
                 "Accept: application/json",
                 "Content-type: application/json"
-                ])
+            ])
 
-        url = URL_ROOT + PATH_SEARCH +"?"+ '&'.join([x+"="+y for x,y in self.params.iteritems()])
+        url = URL_ROOT + PATH_SEARCH +"?"+ '&'.join(
+            [str(x)+"="+str(y) for x,y in self.params.items()])
         self.curl.setopt(pycurl.URL,url)
-        curl.setopt(pycurl.WRITEDATA, buffer)
-        curl.perform()
-        curl.close()
+        self.curl.setopt(pycurl.WRITEDATA, self.buffer)
+        print("Request: "+url)
+        self.curl.perform()
+        self.curl.close()
 
     def getNoticeStream(self):
         if self.buffer:
+            self.buffer.seek(0)
             return NoticeStream(self.buffer)
         else:
-            return null
+            return None
 
 
 class NoticeStream(object):
 
     def __init__(self, buffer):
-        self.buffer = buffer
         self.jsonstream = ijson.items(buffer, 'notices.item')
 
     def __iter__(self):
@@ -58,8 +60,8 @@ class Daemon(object):
 
     def request_time(self, time_in, time_out):
         request = Request(
-                date_received=time_in+".."+time_out,
-                per_page=100
+                date_received=repr(time_in)+".."+repr(time_out),
+                per_page=500
             )
         request.execute()
         # TOOD: make future
@@ -67,9 +69,11 @@ class Daemon(object):
 
 
 if __name__ == '__main__':
-    rightnow =  int(time.time() *10e6)
-    earlier =  int((time.time()-3600) *10e6)
+    rightnow = int((time.time() - 3600 * 60 * 24 * 3) * 10e6)
+    earlier =  int((time.time() - 3600 * 60 * 24 * 4) * 10e6)
     d = Daemon()
     evts = d.request_time(earlier, rightnow)
-    for v in evts:
-        print (v)
+    parsed_evts = (RequestParser.NoticeProcessor(v).get_report() for v in evts)
+    for evt in parsed_evts:
+        if evt :
+            print(evt)
---
+++ src/RequestParser.py
@@ -0,0 +1,99 @@
+from __future__ import division
+import json
+import re
+import operator
+from urllib.parse import urlsplit
+from functools import reduce
+from collections import Counter
+from pprint import pprint
+
+from sys import argv
+import logging
+
+log = logging.getLogger("Parser")
+log.level = logging.DEBUG 
+
+def get_tokens(s, prune=True):
+    ## remove ['.html', '.html', '.php', '.py', 'erb'] etc
+    pruned = re.sub(r'\.\w{1,4}$', '', s, flags=re.IGNORECASE) if prune else s
+    pre_split = re.split(r'\W', pruned)
+    ## remove empty strings
+    clean_split = filter (lambda x : x, pre_split)
+    return set(clean_split)
+
+
+class NoticeProcessor(object):
+    def __init__(self, notice, token_threshold=5, common_proportion=0.25, diversity_threshold = 0.2, rules=None):
+        self.notice = notice
+        self.token_threshold = token_threshold
+        self.common_proportion = common_proportion
+        self.diversity_threshold = diversity_threshold
+        self.rules = rules or {'disallowed_tokens':['index', 'rar', 'download', 'pirate', 'mp3', 'pdf', 'torrent', 'file', '4shared']}
+
+    def apply_token_threshold(self, col):
+        try:
+            # First value of most common values, return the count
+            max_num = col.most_common(1)[0][1]
+
+            def filter_func(x):
+                cond = (x[1] >= max(self.token_threshold, max_num * self.common_proportion)) and (x[0] not in self.rules['disallowed_tokens'])
+                return cond
+
+            large_enough = filter(filter_func, col.items())
+            return list(large_enough)
+        except Exception as e:
+            return []
+
+
+    def make_report(self):
+        ''' 
+        we want to see that the domains 
+        in the list are varied (over space or over time.)
+        First we deal with space.
+        '''
+        col = self.notice['dmca'] if __name__ == "__main__" else self.notice
+        for work in col['works']:
+            try:
+                report = {}
+                urls = (x['url'] for x in work['infringing_urls'])
+                parsed_urls = [urlsplit(x) for x in urls]
+
+                ## Calculate Tokens
+                token_list = (get_tokens(x.path) | get_tokens(x.query) for x in parsed_urls)
+                all_tokens = reduce(operator.add, map(Counter, token_list), Counter())
+     
+                common_tokens = self.apply_token_threshold(all_tokens)
+                if not common_tokens:
+                    continue
+                else:
+                    report['common_tokens'] = common_tokens
+
+                ## Calculate Diversity
+                sites = [x.hostname for x in parsed_urls]
+                num_sites, num_urls = len(set(sites)), len(sites)
+                diversity = num_sites / num_urls
+
+                if diversity < self.diversity_threshold:
+                    continue
+                else:
+                    report['num_urls'] = num_urls
+                    report['num_sites'] = num_sites
+                    report['diversity'] = diversity
+
+                yield report
+            except Exception as e:
+                print (e)
+
+    def get_report(self):
+        return list(self.make_report())
+
+
+def main():
+    filename = argv[1]
+    with open(filename) as f:
+        jf = json.load(f)
+        proc = NoticeProcessor(jf)
+        pprint(proc.get_report())
+
+if __name__ == '__main__':
+    main()
